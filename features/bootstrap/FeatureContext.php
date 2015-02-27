ea70eb9be16b7954701009b8df41203666534b59
defenestrator <jeremyblc@gmail.com>
2015-02-26 21:14:03 -0700

    PhpSpec and Behat test coverage for default classes, features and routes.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
@@ -12,8 +13,21 @@ use PHPUnit_Framework_Assert as PHPUnit;
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context, SnippetAcceptingContext
+
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
 {
+    use Migrator, DatabaseTransactions;
+
+    /**
+     * @var name
+     */
+    protected $name;
+
+    /**
+     * @var email
+     */
+    protected $email;
+
     /**
      * Initializes context.
      *
@@ -23,5 +37,100 @@ class FeatureContext implements Context, SnippetAcceptingContext
      */
     public function __construct()
     {
+
+    }
+
+    /**
+     * @When I register :name :email
+     *
+     */
+    public function iRegister($name, $email)
+    {
+        // dd(app()->environment());
+        $this->name = $name;
+        $this->email = $email;
+
+        $this->visit('auth/register');
+        $this->fillField('name', $name);
+        $this->fillField('email', $email);
+        $this->fillField('password', 'password');
+        $this->fillField('password_confirmation', 'password');
+
+        $this->pressButton('Register');
+
+    }
+
+    /**
+     * @Then I should have an account
+     */
+    public function iShouldHaveAnAccount()
+    {
+        $this->assertSignedIn();
+    }
+
+    /**
+     * @Given I have an account :arg1 :arg2
+     */
+    public function iHaveAnAccount($name, $email)
+    {
+        $this->iRegister($name, $email);
+        $this->visit('auth/logout');
+    }
+
+    /**
+     * @When I sign in
+     */
+    public function iSignIn()
+    {
+        $this->visit('auth/login');
+
+        $this->fillField('email', $this->email);
+        $this->fillField('password', 'password');
+        $this->pressButton('Login');
+    }
+
+    /**
+     * @When I sign in with invalid credentials
+     */
+    public function iSignInWithInvalidCredentials()
+    {
+        $this->visit('auth/login');
+
+        $this->fillField('email', 'invalid@example.com');
+        $this->fillField('password', 'invalid');
+    }
+
+    /**
+     * @Then I should be logged in
+     */
+    public function iShouldBeLoggedIn()
+    {
+        $this->assertSignedIn();
+    }
+
+    /**
+     * @Then I should not be logged in
+     */
+    public function iShouldNotBeLoggedIn()
+    {
+        PHPUnit::assertTrue(Auth::guest());
+    }
+
+    /**
+     * @Then I am signed in
+     */
+    private function assertSignedIn()
+    {
+        PHPUnit::assertTrue(Auth::check());
+
+        $this->assertPageAddress('home');
+    }
+
+    /**
+     * @Given I am signed in
+     */
+    public function iAmSignedIn()
+    {
+        $this->iShouldBeLoggedIn();
     }
 }
